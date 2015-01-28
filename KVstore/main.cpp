6e8415fdffc25cb3faf4d419b34f84d2204b9f2e
Abhijit Khinvasara <abhikhinvasara@gmail.com>
2015-01-28 15:47:16 -0800

    Initial Commit

---
+++ KVstore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KVstore
+//
+//  Created by Abhijit Khinvasara on 1/28/15.
+//  Copyright (c) 2015 Abhijit Khinvasara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
